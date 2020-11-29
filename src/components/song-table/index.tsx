f68a4c5b9ec72ab36f69519d311e367e4e650807
liucannn@sina.com <liucannn@sina.com>
2020-11-29 21:55:47 +0800

    feat: song table

--- src/components/song-table/index.tsx
+++ src/components/song-table/index.tsx
@@ -1,53 +1,57 @@
-import React, { FC } from "react";
-import cx from "classnames";
-import "./index.scss";
-import { msToTime } from "@/utils/time";
+import React, { FC } from 'react';
+import cx from 'classnames';
+import './index.scss';
+import { msToTime } from '@/utils/time';
 
 interface SongTableProps {
   songs: any[];
   className?: string;
+  hideHeader?: boolean;
 }
 
 const columns = [
   {
-    title: "音乐标题",
-    dataIndex: "name",
-    key: "name",
+    title: '音乐标题',
+    dataIndex: 'name',
+    key: 'name',
   },
   {
-    title: "歌手",
-    dataIndex: "ar",
-    key: "ar",
-    render: (ars: { name: string }[]) => ars.map((item) => item.name).join("/"),
+    title: '歌手',
+    dataIndex: 'ar',
+    key: 'ar',
+    render: (ars: { name: string }[]) => ars.map((item) => item.name).join('/'),
   },
   {
-    title: "时长",
-    dataIndex: "dt",
-    key: "dt",
+    title: '时长',
+    dataIndex: 'dt',
+    key: 'dt',
     render: (dt: number) => msToTime(dt),
   },
 ];
 
-const SongTable: FC<SongTableProps> = ({ songs, className }) => {
+const SongTable: FC<SongTableProps> = ({ songs, className, hideHeader }) => {
   return (
-    <table className={cx("song-table", className)}>
-      <thead>
-        <tr>
+    <table className={cx('song-table', className)}>
+      <thead
+        className={cx('table-header', { 'table-header-hide': hideHeader })}>
+        <tr className='table-row'>
           {columns.map((item) => (
-            <th key={item.title}>{item.title}</th>
+            <th className='table-cell' key={item.title}>
+              {item.title}
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
         {songs.map((song) => {
           return (
-            <tr key={song.id}>
+            <tr className='table-row' key={song.id}>
               {columns.map((item) => {
                 return (
-                  <th key={song[item.key]}>
+                  <td className='table-cell' key={song[item.key]}>
                     {item.render?.(song[item.dataIndex]) ??
                       song[item.dataIndex]}
-                  </th>
+                  </td>
                 );
               })}
             </tr>
